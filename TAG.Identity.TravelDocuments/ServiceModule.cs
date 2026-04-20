bd63bece58f3c3ff9a8c795875ba521220e162d7
Peter Waher <peterwaher@hotmail.com>
2026-04-20 17:26:29 -0400

    Validating NFC document

--- TAG.Identity.TravelDocuments/ServiceModule.cs
+++ TAG.Identity.TravelDocuments/ServiceModule.cs
@@ -1,9 +1,16 @@
-﻿using Paiwise;
+﻿using NeuroAccess.Nfc;
+using NeuroAccess.Nfc.TravelDocuments;
+using NeuroAccess.Nfc.TravelDocuments.ISO19794;
+using Paiwise;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Xml;
 using Waher.Events;
 using Waher.IoTGateway;
 using Waher.Runtime.Inventory;
+using Waher.Runtime.Settings;
 
 namespace TAG.Identity.TravelDocuments
 {
@@ -90,10 +97,56 @@ namespace TAG.Identity.TravelDocuments
 		/// <returns>How well objects of this type are supported.</returns>
 		public Grade Supports(IIdentityApplication Identity)
 		{
-			// TODO: Revise the properties in the identity application to see if this identity authenticator service
-			//       is applicable and can validate it.
+			string PreviewId = GetPreviewId(Identity);
+			if (string.IsNullOrEmpty(PreviewId))
+				return Grade.NotAtAll;
 
-			return Grade.NotAtAll;
+			KeyValuePair<XmlDocument, DocumentInformation> P = GetNfcDocument(Identity);
+
+			if (P.Key is null || P.Value is null)
+				return Grade.NotAtAll;
+			else
+				return Grade.Excellent;
+		}
+
+		private static string GetPreviewId(IIdentityApplication Application)
+		{
+			foreach (KeyValuePair<string, object> Claim in Application.Claims)
+			{
+				if (Claim.Key == "PREVIEW" && Claim.Value is string s)
+					return s;
+			}
+
+			return string.Empty;
+		}
+
+		private static KeyValuePair<XmlDocument, DocumentInformation> GetNfcDocument(IIdentityApplication Application)
+		{
+			foreach (XmlDocument Doc in Application.Documents)
+			{
+				if (Doc.DocumentElement is not null &&
+					Doc.DocumentElement.LocalName == "SnifferOutput" &&
+					Doc.DocumentElement.NamespaceURI == "http://waher.se/Schema/SnifferOutput.xsd")
+				{
+					IEnumerator e = Doc.DocumentElement.GetEnumerator();
+
+					if (!e.MoveNext() ||
+						e.Current is not XmlElement E ||
+						E.LocalName != "Info")
+					{
+						continue;
+					}
+
+					string Mrz = IsoDepReplay.GetRows(E).Replace("\r\n", "\n").Replace('\r', '\n').Trim();
+
+					if (!MrzExtensions.ParseMrz(Mrz, out DocumentInformation DocInfo))
+						continue;
+
+					return new KeyValuePair<XmlDocument, DocumentInformation>(Doc, DocInfo);
+				}
+			}
+
+			return new KeyValuePair<XmlDocument, DocumentInformation>(null, null);
 		}
 
 		/// <summary>
@@ -104,7 +157,69 @@ namespace TAG.Identity.TravelDocuments
 		{
 			try
 			{
-				// TODO: Verify application and return corresponding result.
+				KeyValuePair<XmlDocument, DocumentInformation> P = GetNfcDocument(Application);
+				XmlDocument Nfc = P.Key;
+				DocumentInformation DocInfo = P.Value;
+				Representation Face = null;
+
+				if (Nfc is null || DocInfo is null)
+					return;
+
+				// Validate NFC replay document.
+
+				try
+				{
+					IsoDepReplay Replay = new(Nfc);
+					using TravelDocumentsClient Client = new(Replay, Replay.DocumentInfo, null);
+
+					AuthenticateResult AuthResult = await Client.Authenticate();
+					if (AuthResult != AuthenticateResult.Success)
+						throw new Exception("Unable to authenticate NFC document: " + AuthResult.ToString());
+
+					DocumentInformation DocInfo2 = null;
+
+					Client.MrzUpdated += (_, e) =>
+					{
+						DocInfo2 = Client.Mrz.DocumentInformation;
+						return Task.CompletedTask;
+					};
+
+					Client.BiometricEncodingFaceUpdated += (_, e) =>
+					{
+						if (Client.BiometricEncodingFace is not null)
+							Face = Client.BiometricEncodingFace[0].BiometricDataBlock?.Record?.Representations[0];
+
+						return Task.CompletedTask;
+					};
+
+					string OnboardingNeuron = await RuntimeSettings.GetAsync("Onboarding.DomainName", "id.tagroot.io");
+
+					ReadTravelDocumentResult Result = await Client.ReadTravelDocument(OnboardingNeuron);
+					if (Result != ReadTravelDocumentResult.Success)
+						throw new Exception("Unable to read embedded Travel Document: " + Result.ToString());
+
+					if (DocInfo is null)
+						throw new Exception("MRZ not available in embedded Travel Document.");
+
+					if (DocInfo.MRZ_Information.Replace("\n", string.Empty).Replace("\r", string.Empty) !=
+						DocInfo2.MRZ_Information.Replace("\n", string.Empty).Replace("\r", string.Empty))
+					{
+						throw new Exception("MRZ provided in authentication not same as MRZ encoded in Travel Document.");
+					}
+
+					if (Face is null)
+						throw new Exception("Face not available in embedded Travel Document.");
+				}
+				catch (Exception ex)
+				{
+					Log.Exception(ex);
+					Application.InvalidateAllClaims("NFC document invalid.", "en", "NfcInvalid");
+					Application.InvalidateAllPhotos("NFC document invalid.", "en", "NfcInvalid");
+					return;
+				}
+
+				PersonalInformation PersonalInfo = PersonalInformation.Create(Application.Claims);
+
 			}
 			catch (Exception ex)
 			{
